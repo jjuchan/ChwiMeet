c01b5552cbd4c33af5bb29a997617d2b24f2b7dd
jjuchan <yjc7241@naver.com>
2025-12-08 13:50:18 +0900

    test: s3 config 추가

--- backend/src/test/java/com/back/config/TestConfig.java
+++ backend/src/test/java/com/back/config/TestConfig.java
@@ -1,18 +1,26 @@
 package com.back.config;
 
+import static org.mockito.Mockito.*;
+
+import org.mockito.Mockito;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
-import static org.mockito.Mockito.mock;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @TestConfiguration
 public class TestConfig {
 
-    @Bean
-    @Primary
-    public VectorStore testVectorStore() {
-        return mock(VectorStore.class);
-    }
+	@Bean
+	@Primary
+	public VectorStore testVectorStore() {
+		return mock(VectorStore.class);
+	}
+
+	@Bean
+	public S3Client s3Client() {
+		return Mockito.mock(S3Client.class);
+	}
 }
